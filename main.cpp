3e2e298c93ec0bd3ba547c9941293a102908e776
d-uni <84150950+d-uni@users.noreply.github.com>
2022-10-18 11:38:18 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,30 +1,41 @@
-class C {
-    int *p;
-public:
-    C(){}
-    C(int x, int y){}
-    C(const C &x, int y) {}
-    C(double x) {}
-    C(const C *x) {}
-    int & operator [] ( int i) { return p[i]; }
-    operator int() { return 1; }
-    friend int operator + (int a, const C &b);
-    friend int operator ~ (const C &b);
-    friend C operator ++ (C &b);
-    friend int operator * (const C &a, C *b);
-    int operator ~ () {return 1; }
-    ~C(){};
+#include <iostream>
+using namespace std;
 
-};
-int operator + (int a, const C &b) {
-    return 1;
-}
-int operator ~ (const C &b) {
-    return 1;
-}
-C operator ++ (C &b) {
-    return 1;
-}
-int operator * (const C &a, C *b) {
-    return 1;
+int main()
+{
+    char ch;
+    int count = 0;
+    char pr;
+    if (cin.get(pr) && !cin.eof()) {
+        while (cin.get(ch)  && !cin.eof()) {
+            if (pr == ch) {
+                count++;
+            } else {
+                if (count >= 4 || pr == '#') {
+                    cout << '#';
+                    cout << pr;
+                    cout << hex << count + 1 << dec;
+                    cout << '#';
+                } else {
+                    for (int i = 0; i < count + 1; i++) {
+                        cout << pr;
+                    }
+                }
+                count = 0;
+                pr = ch;
+            }
+        }
+            if (count >= 4 || pr == '#') {
+                cout << '#';
+                cout << pr;
+                cout << hex << count + 1 << dec;
+                cout << '#';
+
+            } else {
+                for (int i = 0; i < count + 1; i++) {
+                    cout << pr;
+                }
+            }
+    }
+    return 0;
 }
