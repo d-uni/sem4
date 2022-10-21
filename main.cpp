4ae8020afd5a5ffa5dd811fd51a10ea70204c240
d-uni <84150950+d-uni@users.noreply.github.com>
2022-10-21 18:43:45 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+int main() {
+    string in;
+    while(cin >> in) {
+        stack <char> stack1;
+        for(auto i: in) {
+
+        }
+
+
+    }
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
